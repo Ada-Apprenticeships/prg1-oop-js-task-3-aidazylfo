2fd37c63e9087212554d664e04805d6e23d82b4c
aidazylfo <aida.zylfo@ada.ac.uk>
2024-10-25 14:10:32 +0000

    intresting

--- oop.js
+++ oop.js
@@ -2,16 +2,30 @@ PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
 function validInteger (value) { // value can be a string or a number (integer)
-  
+  function returnBoolean(input) {
+    return input === true;
 }  
 
+console.log(returnBoolean(true));  // Outputs true
+console.log(returnBoolean(false)); // Outputs false
+}
 
 function validatePriority(priority) { // value can be a string or a number (integer)
-  
+  function returnBoolean(input) {
+    return input === true;
+}
+console.log(returnBoolean(true));  // Outputs true
+console.log(returnBoolean(false)); // Outputs false
+
 }
 
 
 function todaysDate () {
+  function returnBoolean(input) {
+    return input === true;
+}
+console.log(returnBoolean(true));  // Outputs true
+console.log(returnBoolean(false)); // Outputs false
   
 }
 
